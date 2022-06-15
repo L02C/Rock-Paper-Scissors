4ee6c1484d753d4fe09bcdfeda91078d440c9cca
Lidor <101148504+L02C@users.noreply.github.com>
2022-06-15 15:22:17 +0200

    Update UI and gaming system

--- script.js
+++ script.js
@@ -1,101 +1,146 @@
+let computerScore = 0;
+let playerScore = 0;
+
 function computerPlay() {
   computerSelections = ['Rock', 'Paper', 'Scissors'];
   const computerSelection = computerSelections[Math.floor(Math.random() * computerSelections.length)];
   return computerSelection
 }
 
-function game() {
-  for (let i = 1; i <6; i++) {
-    const playerSelection = prompt(`Round ${i}: rock, paper, or scissors?`);
-    const computerSelection = computerPlay();
-    const roundResults = (playRound(playerSelection, computerSelection));
-    console.log(roundResults);
-    if (roundResults == 'Please enter a valid input.') {
-      i += -1; // re-play the round
+function checkscore() {
+  if (playerScore == 5 || computerScore == 5) {
+    if (playerScore > computerScore) {
+      main_message.innerHTML = (`Congratulations! You won this game of Rock Paper Scissors.`);
+      rock_btn.removeEventListener("click", rock);
+      paper_btn.removeEventListener("click", paper);
+      scissors_btn.removeEventListener("click", scissors);
+      round_message.innerHTML = (`Click 'New Game' to play again.`);
+    }
+
+    else if (computerScore == playerScore) {
+      main_message.innerHTML = (`You tied this game of Rock Paper Scissors.`);
+      rock_btn.removeEventListener("click", rock);
+      paper_btn.removeEventListener("click", paper);
+      scissors_btn.removeEventListener("click", scissors);
+      round_message.innerHTML = (`Click 'New Game' to play again.`);
+    }
+
+    else if (computerScore > playerScore) {
+      main_message.innerHTML = (`Sorry! You lost this game of Rock Paper Scissors.`);
+      rock_btn.removeEventListener("click", rock);
+      paper_btn.removeEventListener("click", paper);
+      scissors_btn.removeEventListener("click", scissors);
+      round_message.innerHTML = (`Click 'New Game' to play again.`);
     }
   }
-  if (playerScore == computerScore) {
-    console.log(`You tied this game of Rock Paper Scissors.
-Your score: ${playerScore}.
-Computer's score: ${computerScore}`);
-  }
-
-  else if (playerScore > computerScore) {
-    console.log(`Congratulations! You won this game of Rock Paper Scissors.
-Your score: ${playerScore}.
-Computer's score: ${computerScore}`);
-  }
-
-  else if (playerScore < computerScore) {
-    console.log(`Sorry! You lost this game of Rock Paper Scissors.
-Your score: ${playerScore}.
-Computer's score: ${computerScore}.`);
-  }
-}
+};
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection.toLowerCase().trim() == 'rock') {
+  if (playerSelection == 'Rock') {
     if (computerSelection == 'Rock') {
       const message = "You tie this round! Rock and rock ties."
-      playerScore += 1;
-      computerScore += 1;
+      player_score_UI.innerHTML = (playerScore += 1);
+      computer_score_UI.innerHTML = computerScore += 1;
       return message;
     }
     if (computerSelection == 'Paper') {
       const message = "You lose this round! Paper beats rock."
-      computerScore += 1;
+      computer_score_UI.innerHTML = computerScore += 1;
       return message;
     }
     if (computerSelection == 'Scissors') {
       const message = "You win this round! Rock beats scissors."
-      playerScore += 1;
+      player_score_UI.innerHTML = playerScore += 1;
       return message;
     }
   }
 
-  else if (playerSelection.toLowerCase().trim() == ('paper')) {
+  else if (playerSelection == ('Paper')) {
     if (computerSelection == 'Rock') {
       const message = "You win this round! Paper beats rock."
-      playerScore += 1;
+      player_score_UI.innerHTML = playerScore += 1;
       return message;
     }
     if (computerSelection == 'Paper') {
       const message = "You tie this round! Paper and paper ties."
-      playerScore += 1;
-      computerScore += 1;
+      player_score_UI.innerHTML = playerScore += 1;
+      computer_score_UI.innerHTML = computerScore += 1;
       return message;
     }
     if (computerSelection == 'Scissors') {
       const message = "You lose this round! Scissors beats paper."
-      computerScore += 1;
+      computer_score_UI.innerHTML = computerScore += 1;
       return message;
     }
   }
 
-  else if (playerSelection.toLowerCase().trim() == ('scissors')) {
+  else if (playerSelection == ('Scissors')) {
     if (computerSelection == 'Rock') {
       const message = "You lose this round! Rock beats scissors."
-      computerScore += 1;
+      computer_score_UI.innerHTML = computerScore += 1;
       return message;
     }
     if (computerSelection == 'Paper') {
       const message = "You win this round! Sccisors beats paper."
-      playerScore += 1;
+      player_score_UI.innerHTML = playerScore += 1;
       return message;
     }
     if (computerSelection == 'Scissors') {
       const message = "You tie this round. Scissors and scissors tie."
-      playerScore += 1;
-      computerScore += 1;
+      player_score_UI.innerHTML = playerScore += 1;
+      computer_score_UI.innerHTML = computerScore += 1;
       return message;
     }
-  } else {
-    const message = 'Please enter a valid input.';
-    return message
   }
+};
+
+const new_game = document.getElementById('new-game-btn');
+const main_message = document.getElementById('main-message');
+const round_message = document.getElementById('round-message');
+const computer_score_UI = document.getElementById('computer-score');
+const player_score_UI = document.getElementById('player-score');
+const rock_btn = document.getElementById('rock-btn');
+const paper_btn = document.getElementById('paper-btn');
+const scissors_btn = document.getElementById('scissors-btn');
+
+function newgame() {
+  console.log(`Let's play!`);
+  computerScore = 0;
+  playerScore = 0;
+  player_score_UI.innerHTML = '0';
+  computer_score_UI.innerHTML = '0';
+  main_message.innerHTML = 'New game started!';
+  round_message.innerHTML = 'First to 5 wins!';
+  rock_btn.addEventListener("click", rock);
+  paper_btn.addEventListener("click", paper);
+  scissors_btn.addEventListener("click", scissors);
 }
 
-console.log(`Let's play!`);
-let computerScore = 0;
-let playerScore = 0;
-game();
+function rock() {
+  playerSelection = "Rock";
+  const computerSelection = computerPlay();
+  const roundResults = (playRound(playerSelection, computerSelection));
+  console.log(roundResults);
+  round_message.innerHTML = (roundResults);
+  checkscore();
+}
+
+function paper() {
+  playerSelection = "Paper";
+  const computerSelection = computerPlay();
+  const roundResults = (playRound(playerSelection, computerSelection));
+  console.log(roundResults);
+  round_message.innerHTML = (roundResults);
+  checkscore();
+}
+
+function scissors() {
+  playerSelection = "Scissors";
+  const computerSelection = computerPlay();
+  const roundResults = (playRound(playerSelection, computerSelection));
+  console.log(roundResults);
+  round_message.innerHTML = (roundResults);
+  checkscore();
+}
+
+new_game.addEventListener("click", newgame);
